d06e72d8ab77b93a2f8c229c813ebafae2b60d5e
Ihor Melnyk <igor.melnick85@gmail.com>
2023-10-24 10:04:47 +0300

    added initial files

---
+++ index.js
@@ -0,0 +1,5 @@
+function setAttrValue(attributeCode, attributeValue) {
+  var attribute = EdocsApi.getAttributeValue(attributeCode);
+  attribute.value = attributeValue;
+  EdocsApi.setAttributeValue(attribute);
+}
